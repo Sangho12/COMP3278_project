deb20d8c34535c2579abca63c8e0feca3daf97c9
tlyee <163804483+TLYEE@users.noreply.github.com>
2026-04-06 21:03:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,13 +54,56 @@ def index():
   return redirect(url_for('login'))
 
 # user authentication - login
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    
+    db = get_db()
+    user = db.execute("SELECT * FROM User WHERE username = ? AND password = ?", (username, hashed_password)).fetchone()
+    
+    if user:
+      session['username'] = user['username']
+      return redirect(url_for('index'))
+      
+    else:
+      flash("Invalid username or password.")
+  return render_template('login.html')
+      
 
 # user authentication - register
 
-
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    if not username or not password:
+      flash("Incorrect username or password.")
+      return redirect(url_for('register'))
+      
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    db = get_db()
+    
+    try:
+      db.execute("INSERT INTO User (username, password) VALUES (?, ?)", (username, hashed_password))
+      db.commit()
+      flash("Registration successful! Please go to log in.")
+      return redirect(url_for('login'))
+      
+    except sqlite3.IntegrityError:
+      flash("Username already exists! Try another one.")
+  return render_template('register.html')
 # user authentication - logout
-
+@app.route('/logout')
+def logout():
+  session.pop('username', None)
+  flash("You have been logged out.")
+  return redirect(url_for('login'))
 
 # Posts
 
